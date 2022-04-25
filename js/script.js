a4cccf8580c127da95313a92e96bb369e6723ce9
jduncan1222 <jduncan1222@gmail.com>
2022-04-25 13:08:52 -0400

    init

--- js/script.js
+++ js/script.js
@@ -2,10 +2,24 @@ const API_KEY = '6ae6cd0fc1b2ed3670ea82c17b067a14';
 const BASE_URL = 'https://api.themoviedb.org/3/movie/popular?api_key=';
 const IMG_URL = 'https://image.tmdb.org/t/p/w500';
 const API_URL = BASE_URL + API_KEY;
+const API_SEARCH = 'https://api.themoviedb.org/3/search/movie?api_key=';
+const SEARCH_EL = document.querySelector('#searchInput');
+
 let main = document.querySelector('#main');
 
+SEARCH_EL.addEventListener('keydown', (e) => {
+        
+        let searchText = document.querySelector('#searchInput').value;
+        let searchURL = API_SEARCH + API_KEY + '&query=' + searchText;
+        fetch(searchURL).then(response => response.json().then(data => {
+            console.log('fetched');
+        }))
+})
 
 getData(API_URL);
+
+
+
 
 function getData(url) {
     fetch(url).then(response => response.json()).then(data => {
